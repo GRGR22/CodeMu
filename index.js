11dd765b5f2021410ff8a052db491ab557731883
Gennadiy Bazarnov <bagbox@mail.ru>
2021-09-10 12:00:03 +0400

    func manip

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-// let arr = [2,5,9]
-// let math = {
-//     summOfArrElem: function(array) {
-//          return array.reduce((a, b) => a + b, 0)
-//     },
-// 	summOfArrsquare: function(array) {return array.reduce((a, b) => a + b*b, 0)},
-// 	cube: function(array) {return array.reduce((a, b) => a + Math.pow(b, 3), 0)},
-// };
-// console.log( math.summOfArrElem(arr) );
-// console.log( math.summOfArrsquare(arr) ); 
-// console.log( math.cube(arr) );
+// function get1() {
+// 	return 1;
+// }
+// function get2() {
+// 	return 2;
+// }
 
-test(
-	function() {return 1;},
-	function() {return 2;},
-    function() {return 4;}
-);
+// test(get1, get2); // выведет 3
 
-function test(func1, func2, func3) {
-	alert( func1() + func2() + func3());
+// function test(func1, func2) {
+// 	alert( func1() + func2() );
+// }
+let get1 = function() {
+	return 1;
+}
+let get2 = function() {
+	return 2;
+}
+
+test(get1, get2); // выведет 3
+
+function test(func1, func2) {
+	alert( func1() + func2() );
 }
