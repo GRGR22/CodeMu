19bdd08ee00f6ea401be0be0a7ce92fb4bd34e34
Gennadiy Bazarnov <bagbox@mail.ru>
2021-09-15 16:44:10 +0400

    Closures - замыкания

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-function func(num1) {
-	return function(num2) {
-		return function(num3) {
-			return function(num4) {
-				return function(num5) {
-					let arr = [];
-					for (let index = 0; index < 5; index++) {						
-						eval('arr[' + index + '] = num' +(index+1))	
-					};
-					return arr;
-				};
-			};	
-		};
-	};
-}
 
-console.log (func(2)(3)(4)(5)());
+function func() {
+	let count = 10;
+	return function counting () {
+		if (count > 0) { 
+			alert (count);
+			count--;
+		} else { 
+			alert ('count<=0');
+		}						
+	}
+}
+let countOfFuncCall = func();
+while (true) {
+	countOfFuncCall ();
+}
