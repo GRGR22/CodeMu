e79b1ad010bd665917b2d204e28387a3163f9351
GRGR22 <bagbox@mail.ru>
2021-10-12 14:29:42 +0400

    iife counter

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// (function(num1, num2) {
-// 	let sum = num1 + (function(n) {
-// 	return n;
-// })(num2);
-// return sum;
-// })(1)(2);
-(function(v) {	
-	return function(n) {
-		return function(z) {
-			console.log(n+v+z);
-		};
-	};
-})(1)('dfg')('dfg');
+let func = (function() {
+	let num = 1;
+	
+	return function() {
+		alert(num);
+		num++;
+		num>5 ? num = 0: 0;
+	}
+})();
+for (i=0; i<10; i++) {
+	func();
+};
