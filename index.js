897e3e5ec88c76205b913f57b258c8f294e877ad
GRGR22 <bagbox@mail.ru>
2021-10-14 13:16:47 +0400

    recursion+

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-let arr = [1, 2, 3, 4, 5];
-
-function getSum(arr) {
-	let sum = arr.shift()**2;
-	
-	if (arr.length !== 0) {
-		sum += getSum(arr);
+function func(obj) {
+	for (let elem in obj) {
+		if (typeof obj[elem] == 'object') {
+			func(obj[elem]);
+		} else {
+			console.log(elem, obj[elem]);
+		}
 	}
-	
-	return sum;
 }
 
-console.log(getSum(arr));
+func({a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7, m: {n: 8, o: 9}}}});
