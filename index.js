23056873c0a0a575558c088e77d284610d385c7e
GRGR22 <bagbox@mail.ru>
2021-10-14 14:12:09 +0400

    map2

--- index.js
+++ index.js
@@ -5,11 +5,29 @@
 // });
 // console.log(arr, result);
 
-let arr = ['123', '456', '789'];
+// let arr = ['123', '456', '789'];
 
-let result = arr.map(function(elem) { 
-	return elem.split('').map(function(str) {
-		return +str;
+// let result = arr.map(function(elem) { 
+// 	return elem.split('').map(function(str) {
+// 		return +str;
+// 	});
+// });
+// console.log(arr, result);
+
+// let arr = [2, 4, 6, 8, 10];
+
+// let result = arr.map(function(elem, index) {
+// 	return elem*index;
+// });
+
+// console.log(result); 
+
+let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+let result = arr.map(function(elem) {
+	return elem.map(function(num) {
+		return num * num;
 	});
 });
-console.log(arr, result);
+
+console.log(result);
