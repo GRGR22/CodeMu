ca91d399748cf2467131049356228e245886ea9e
GRGR22 <bagbox@mail.ru>
2021-10-15 12:18:26 +0400

    every2

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
 // let result = arr.every(elem => elem % 2 == 0);
 // console.log(result); // true
 
-let arr = [-2, 4, 6, 8];
+let arr = [-2, 4, 6, 5];
 
-console.log(arr.every(elem => elem > 0));
+console.log(arr.every(function(el, idx) {
+	console.log(el*idx)
+	return el*idx > 30;
+} ));
