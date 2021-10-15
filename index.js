b2e30c0d90f304551071ff033bbac9e48836d8fc
GRGR22 <bagbox@mail.ru>
2021-10-15 12:10:01 +0400

    every

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-//filter возвращает новый массив, в который войдут только те элементы, для которых коллбэк вернет true.
+//every возвращает true, если для всех элементов массива коллбэк вернул true
 
-// let arr = [1, 2, 3, 4, 5];
+// let arr = [2, 4, 6, 8];
 
-// let result = arr.filter(function(elem) {
-// 	return elem % 2 == 0;
-// });
+// let result = arr.every(elem => elem % 2 == 0);
+// console.log(result); // true
 
-// let arr = ['-1', '222222', '33333', '444', '55555555555'];
-// console.log( arr.filter(elem => elem.length > 5) );
+let arr = [-2, 4, 6, 8];
 
-// let arr = [1, 2, 3, 14, 50];
-
-// console.log( arr.filter(function (el, idx) {
-// 	return el*idx > 30
-// }));
-
-// let arr = [1, 2, [3, 4], 5, [6, 7]];
-// console.log( arr.filter(elem => !Array.isArray(elem)) );
-
-// let arr = [1, 2, 3, 4, 5];
-// console.log( arr.filter(elem => elem % 2 == 0).length ); //кол-во четн элем
-let arr = [1, -2, -3, -4, 5];
-console.log( arr.filter(elem => elem < 0).length );
+console.log(arr.every(elem => elem > 0));
