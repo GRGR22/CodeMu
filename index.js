b819fb82d094b4d3cc05a0531ccc08a29a6a0d58
GRGR22 <bagbox@mail.ru>
2021-10-15 13:02:18 +0400

    some

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-//every возвращает true, если для всех элементов массива коллбэк вернул true
+//some возвращает true, если хотя бы для одного элемента коллбэк вернул true
+let arr = [2, 4, 6, 8];
 
-// let arr = [2, 4, 6, 8];
-
-// let result = arr.every(elem => elem % 2 == 0);
-// console.log(result); // true
-
-let arr = [-2, 4, 6, 5];
-
-console.log(arr.every(function(el, idx) {
-	console.log(el*idx)
-	return el*idx > 30;
-} ));
+console.log(arr.some(elem => elem > 0));
