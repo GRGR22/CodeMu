df571f2c806d18361efb7c34f902c838a85567a7
GRGR22 <bagbox@mail.ru>
2021-10-18 16:26:58 +0400

    destructuring object

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-// let arr = [1, 2, 3, 4, 5];
-// function func(num1, num2, num3, num4, num5) {
-// 	return num1 + num2 + num3 + num4 + num5;
-// }
-// console.log(func(...arr))
-let arr = [1, 2, 3, 4, 5];
-console.log(Math.min(...arr));
+// let options = {
+// 	color: 'red',
+// 	width:  400,
+// 	height: 500,
+// };
+// let {color, width, height} = options;
+// console.log(color, width, height);
+
+// let obj = {
+// 	year:  2025,
+// 	month: 12,
+// 	day:   31,
+// };
+
+// let {year: y, month: m, day: d} = obj; //присвоение других имен
+// console.log(y, m, d); // выведет 2025, 12, 31
+
+// let options = {
+// 	width:  400,
+// 	height: 500,
+// };
+// let {color:с = 'black', width:w, height:h} = options;
+
+function func() {
+	return obj = {
+		year:  2025,
+		month: 12,
+		day:   31,
+	};
+}
+let {year, month, day} = func();
