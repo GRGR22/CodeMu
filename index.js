1f8d7016d8f9ada92e3209e750f17e0bcc478cc8
GRGR22 <bagbox@mail.ru>
2021-10-19 16:35:47 +0400

    DOm2

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-let elem1   = document.querySelector('#elem1');
-let button   = document.querySelector('#btn');
+// let button = document.querySelector('#button');
+// let paragraph   = document.querySelector('.paragraph');
+// let link   = document.querySelector('.link');
 
-button.addEventListener('click', function() {
-	elem1.innerHTML = `<b>${elem1.innerHTML}</b>`;
-});
+// button.addEventListener('click', function() {
+// 	paragraph.innerHTML = `(${link.href})`;
+// })
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let paragraph = document.querySelector('.paragraph');
+let picture1 = document.querySelector('.picture1');
+let picture1_src = picture1.src;
+let picture2 = document.querySelector('.picture2');
+let picture2_src = picture2.src;
+
+button1.addEventListener('click', function() {
+	picture1.src = picture2_src;
+})
+button2.addEventListener('click', function() {
+	picture2.src = picture1_src;
+})
