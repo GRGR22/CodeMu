031a128ead27a1d460a21cde0fbb3c41645dcd19
GRGR22 <bagbox@mail.ru>
2021-10-19 18:37:31 +0400

    className, classList

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-// ****** Text field *******
-
-// let elem = document.querySelectorAll('#elem');
-// let result = document.querySelector('#result')
-// let btn = document.querySelector('#button1');
-
-// btn.addEventListener('click', function () {
-//     let res = 0;
-//     for (const i of elem) {        
-//        res += +i.value 
-//     }
-//     result.innerHTML = res/elem.length
-// })
-
-// ******* Focus *******
-
 let elem = document.querySelector('#elem');
-elem.addEventListener('focus', function() {
-	elem.value = '';
-});
-// elem.addEventListener('focusout', function() {
-// 	alert (elem.value**2);
-// });
+let btn = document.querySelector('#button1');
+
+btn.addEventListener('click', function() {
+    alert(elem.className)
+    // elem.classList.add ('ertyu')
+    alert (elem.classList)
+})
