ccee652f59849c13c2c64e0add3466f1ffd865d1
GRGR22 <bagbox@mail.ru>
2021-10-19 16:35:31 +0400

    DOM

--- index.js
+++ index.js
@@ -1,46 +1,6 @@
-// let date = new Date();
-// console.log (date.getMonth())
-// let day  = date.getDay();
-// let days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+let elem1   = document.querySelector('#elem1');
+let button   = document.querySelector('#btn');
 
-// console.log(days[day]);
-
-// let months = [
-// 	'янв', 'фев', 'мар', 'апр', 'май', 'июн',
-// 	'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'
-// ];
-// console.log(months[date.getMonth()])
-
-// let date = new Date(1979, 4+1, 9+1); // установим 9 апр 1979 года
-// let day  = date.getDay()+1;
-// let days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
-
-// console.log(days[day]);
-
-// let date = new Date(2015, 11, 4, 23, 59, 59);
-// console.log(date.getTime()); //timestamp at ms;
-
-// let now  = new Date();
-// let date = new Date(2015, 11, 4, 23, 59, 59);
-
-// let diff = now.getTime() - date.getTime();
-// console.log(diff); // diff time at ms
-
-// let date = new Date(2020, 2, 0);
-
-// if (date.getDate() == 29) {
-// 	console.log('високосный');
-// } else {
-// 	console.log('обычный');
-// }
-// let year  = 2025;
-// let month = 0;
-// let day   = 32;
-
-// let date = new Date(year, month, day);
-
-// if (date.getFullYear() == year && date.getMonth() == month && date.getDate() == day) {
-// 	console.log('корректна');
-// } else {
-// 	console.log('некорректна');
-// }
+button.addEventListener('click', function() {
+	elem1.innerHTML = `<b>${elem1.innerHTML}</b>`;
+});
