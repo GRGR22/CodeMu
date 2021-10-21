6127a3012d92d800c1e2d1dee828d2ce823731d9
GRGR22 <bagbox@mail.ru>
2021-10-21 12:03:30 +0400

    dom work 4

--- index.js
+++
@@ -1,8 +0,0 @@
-let elem = document.querySelector('#elem');
-let btn = document.querySelector('#button1');
-
-btn.addEventListener('click', function() {
-    alert(elem.className)
-    // elem.classList.add ('ertyu')
-    alert (elem.classList)
-})
